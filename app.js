59e215eaa251970b867d5c9ec8824cc669827a95
AnHawk <104738714+AnHawk@users.noreply.github.com>
2025-02-26 14:47:28 +0200

    comit

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+// function toggleMenu() {
+//     const menu = document.getElementById('menu');
+//     menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+// }
+
 function toggleMenu() {
-    const menu = document.getElementById('menu');
-    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+    document.getElementById('menu').classList.toggle('show');
 }
 
+// Открытие подкатегорий на мобильных
+function toggleDropdown(event) {
+    event.preventDefault(); // Отменяем переход по ссылке
+    let submenu = event.target.nextElementSibling;
+    if (submenu.style.display === 'block') {
+        submenu.style.display = 'none';
+    } else {
+        submenu.style.display = 'block';
+    }
+}
